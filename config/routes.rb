ed7bd4bbf6d952713328c479a1bc12ca385edd1a
Shinnosuke Yoshizawa <pokopanic19@gmail.com>
2017-08-28 23:33:48 +0900

    api_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs, only:[:index]
-  resources :poems, only:[:index]
+  resources :poems, only:[:index, :show]
   root to: 'top#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
